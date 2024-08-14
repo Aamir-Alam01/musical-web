26e9949076bce8399ca5284d2905d6b952f55c89
Aamir-Alam01 <aamir060901@gmail.com>
2024-08-15 00:37:16 +0530

    Initial File Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-full">
+      <h1 className="flex justify-center items-center">Hello World</h1>
+    </main>
+  );
+}
