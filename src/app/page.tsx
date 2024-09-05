44bdae27606fbf44f7aab13d86e37c0af7139362
Aamir-Alam01 <aamir060901@gmail.com>
2024-09-05 16:38:20 +0530

    Hero Section Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.05]">
-      <h1 className="flex justify-center items-center">Hello World</h1>
+      <HeroSection />
     </main>
   );
 }
