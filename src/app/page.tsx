61b800118bddd9cb4f820ea8a1e89572190dfacf
Aamir-Alam01 <aamir060901@gmail.com>
2024-09-05 11:48:17 +0530

    Navbar Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="h-full">
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.05]">
       <h1 className="flex justify-center items-center">Hello World</h1>
     </main>
   );
